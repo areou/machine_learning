cc950858ccf8ac393adeb077c8714d72074e4b41
areou <vandermolen.r@gmail.com>
2019-12-09 16:19:47 -0500

    added the observer file for 71

---
+++ the_observer.py
@@ -0,0 +1,33 @@
+from knowledge_cleaning import*
+import math
+
+from keras.datasets import cifar10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+
+happy=True
+
+while happy:
+	which=input('Which one do you want?\n type 71 for the fastest model\n type conv for the slow convolution\n type conv_0 for the faster convolution model')
+
+	if which==71:
+		config=unpickle(robfar_10_config_71)
+		weights=unpickle(robfar_10_best_weights_71)
+		happy=False
+		
+		
+	else:
+		print('sorry not ready for that response yet...\n try again')
+	
+
+# Now change 'data' for what ever data you would like to test out as it stands I just use the testing data for cifar10	
+
+data=(X_test,y_test)
+
+model = Sequential.from_config(config)
+
+model.set_weights(weights)
+
+the_deal = model.evaluate(X_train, yy)
+
+print('Your Data Accuracy: '+str(math.floor(the_deal[1]*100))+'%')
